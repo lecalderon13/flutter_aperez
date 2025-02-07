e7187e65701c2b570125a6422ad51171ddfd9444
Alex Perez Soriano <alexadrianperezsorianoups@gmail.com>
2025-02-06 22:05:11 -0500

    Avance Unidad 9

--- CURSO 1/Seccion 9/ProyectoN1/widgets_app/lib/config/router/app_router.dart
+++ CURSO 1/Seccion 9/ProyectoN1/widgets_app/lib/config/router/app_router.dart
@@ -1,4 +1,3 @@
-
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
 import 'package:widgets_app/config/presentation/screens/cards/card_screen.dart';
@@ -7,24 +6,20 @@ import 'package:widgets_app/config/presentation/screens/home/home_screen.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    
     GoRoute(
       path: '/',
-      name:HomeScreen.name,
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
-
     ),
-    GoRoute( 
+    GoRoute(
       path: '/buttons',
-      name:ButtonsScreen.name,
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
-
     ),
     GoRoute(
       path: '/cards',
-      name:CardScreen.name,
+      name: CardScreen.name,
       builder: (context, state) => const CardScreen(),
-
     ),
-  ],   
+  ],
 );
